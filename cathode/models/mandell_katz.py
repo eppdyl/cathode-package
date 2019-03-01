6b93d41a7d722d09139d968c970261a37607ca5b
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2019-03-01 11:44:15 -0500

    Implement part of main solve function; Implement goal_function; Remove useless code

--- cathode/models/mandell_katz.py
+++ cathode/models/mandell_katz.py
@@ -22,205 +22,6 @@ import numpy as np
 from scipy.optimize import root
 from itertools import product
 
-
-
-def goal_function(X,args):
-    
-    q = cc.elementary_charge
-    kb = cc.Boltzmann
-    
-    # Rescale
-    ne = X[0]*1e20
-    ng = X[1]*1e20
-    phi_p = X[2]
-    Tc = X[3]*1e3
-
-    goal = np.zeros(4)
-    
-    # Unpack arguments    
-    dc, eps_i, mass, phi_wf, DRD, TeV, P, Id, lambda_pr, qth = args
-    
-    # Effective length, areas
-    rc = dc/2
-    lpr = lambda_pr(ne,ng,phi_p)
-    Leff = 2*lpr
-
-    Ae = 2*np.pi*rc*Leff
-    Ac = np.pi*rc**2
-    As = 2*Ac + Ae
-
-    # Power loss per unit length (W/m)
-    qth_val = qth(Tc-273.15) # W/mm
-    qth_val = qth_val*1e3 # W/m
-    
-    # Ion Current density, ion current
-    ji = q*ne*np.sqrt(q*TeV/mass)
-    Ii = ji*As
-
-    # Electron current
-    Ie = jth_rd(DRD,Tc,phi_wf,True,ne,phi_p,TeV)*Ae
-
-    # Equations
-    # Perfect gas law
-    goal[0] = P - kb*(ne*(TeV*q/kb + Tc) + ng*Tc)
-
-    # Current conservation
-    goal[1] = Id-Ie-Ii
-
-    # Insert surface power balance
-    goal[2] = ji*Ae*(phi_p + eps_i - phi_wf) 
-    goal[2] -= qth_val * Leff 
-    goal[2] -= Ie*phi_eff(ne,phi_p,TeV,phi_wf)
-
-    # Plasma power balance
-    goal[3] = phi_p*Ie - eps_i * Ii - 5/2*TeV*Id
-
-    return goal
-
-def sw_pressure_correlation(mdot,Id,orifice_diameter,mass):
-    """
-    Function: sw_pressure_correlation
-    Calculates the pressure correlation proposed by Siegfried and Wilbur
-    """
-    amu_mass = mass/cc.atomic_mass
-    if amu_mass == cc.M.Hg:
-        c1 = 13.7
-        c2 = 7.82
-    elif amu_mass == cc.M.Xe:
-        c1 = 9.0
-        c2 = 4.0
-    elif amu_mass == cc.M.Ar:   
-        c1 = 5.6
-        c2 = 1.2
-
-    Ptorr = mdot / (orifice_diameter*1e3)**2 * (c1 + c2*Id)*1e-3
-    
-    return Ptorr * cc.Torr
-
-def solve(cathode_diameter, 
-          eps_i, mass,
-          phi_wf, DRD,
-          TeV, 
-          P = None, mdot = None, orifice_diameter = None,
-          Id = None, Pfunc = sw_pressure_correlation,
-          lambda_pr = hg_lambda_pr, qth = None,
-          solver_tol = 1E-8,solver_out = False):
-    """
-    Solves for the electron and neutral densities, plasma potential, and
-    neutral gas temperature. Uses:
-        1. Perfect gas law
-        2. Current balance
-        3. Insert surface power balance
-        4. Plasma volume power balance
-    Inputs:
-        1. Geometry: cathode_diameter, orifice_diameter (optional). Both in m
-        2. Gas: ionization potential "eps_i" (eV), particle mass "mass" (kg)
-        3. Emitter info: work function "phi_wf" (eV), Richardson-Dushman 
-        constant for the material considered "DRD" (A/m^2)
-        4. Experimental info: electron temperature "TeV" (eV)
-        5. Operating conditions: either the total pressure "P" (Pa) or the 
-        discharge current "Id" (A) and mass flow rate "mdot" (milli-eqA)
-        6. Necessary functions: a pressure correlation "Pfunc" if Id and mdot 
-        are used to determine the total pressure, an evaluation of the mean 
-        free path for primary electrons "lambda_pr", and an estimate of the 
-        heat loss "qth"
-    """   
-    ### Sanity checks...
-    if (P is None) and (mdot is None) and (Id is None):
-        raise ValueError("Either [P] or [mdot AND Id] must be specified")
-        
-    if (P is not None) and ((mdot is not None) or (Id is not None)):
-        raise ValueError("Only [P] or [mdot and Id] can be specified. Not both")
-   
-    ### Constants
-    q = cc.elementary_charge # Electron charge 
-    kb = cc.Boltzmann
-
-    ### Id case
-    if (Id is not None) and (mdot is not None):
-        if(len(Id) == len(mdot)):
-            cases = zip(Id,mdot)
-        else:
-            cases = product(Id,mdot)
-        
-        solvec = []
-        for lId,lmdot in cases:
-            lP = Pfunc(lmdot,lId,orifice_diameter,mass)
-            
-            # Initial guess: densities and temperature are scaled
-            Tc0 = 1050 + 273.15 # Wall temperature, K
-            ne0 = 1.5e20 # Electron density, 1/m3
-            nc0 = 1./(kb*Tc0)*(lP - ne0*kb*(TeV*q/kb + Tc0)) # Neutral density 
-            # from perfect gas law, 1/m3
-            phi_p0 = 7 # Plasma potential, V
-            
-            x0 = [ne0*1e-20,nc0*1e-20,phi_p0,Tc0*1e-3]
-    
-            # Optimizer options
-            root_options = {'maxiter':int(1e6),
-                            'xtol':solver_tol,'ftol':solver_tol}
-
-            # Arguments
-            dc = cathode_diameter
-            args = [dc, eps_i, mass, phi_wf, DRD, TeV, lP, lId, lambda_pr, qth]
-            
-            # Solve!
-            optimize_results = root(goal_function,x0,args=args,
-                                    method='lm',options = root_options)
-            
-            # Extract and rescale results
-            ne,nc,phi_p,Tc = optimize_results.x
-            
-            rescaled_results = [ne*1e20,nc*1e20,phi_p,Tc*1e3-273.15]
-            
-            solvec.append(rescaled_results)
-            
-    return np.array(solvec)
-
-
-
-
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 25 15:51:24 2017
-
-@author: Arbiter
-"""
-
-from __future__ import division # Because reasons.
-import numpy as np
-from scipy.optimize import fsolve,root
-import matplotlib.pyplot as plt
-import scipy.constants as cs
-
-
-### Constants
-cm = cs.centi
-angstrom = 1E-10
-e = cs.elementary_charge
-me = cs.electron_mass
-kB = cs.k
-pi = np.pi
-eps0 = cs.epsilon_0
-
-### Cathode info
-r = 0.0368*cm
-L = 0.076*cm
-
-#r = 0.014*cm
-#L = 0.075*cm
-### Gas info
-E_i = 12.2
-E_rad = 10.0
-T_n = 0.1 
-M = cs.u*131.293 # Mass of Xe, kg
-
-### Experimental data
-T_e_ins = 0.8 # Insert electron temp., eV
-#T_e_ins = 0.46 # Insert electron temp., eV
-
-
-
 def sigma_iz(T_e):
     return (3.97+0.643*T_e-0.0368*T_e**2)*np.exp(-12.127/T_e)*angstrom**2
 
@@ -279,14 +80,25 @@ def ion_balance(n_e,T_e,N_n):
 def flow_balance(n_e,N_n,T_e,F):
     return pi*r**2*e*N_n*np.sqrt(e*T_n/(2*pi*M))-(0.0718*F-pi*r**2*J_i(n_e,T_e))
 
-def zerofun(x,T_e_ins,I_d,F):
+def goal_function(x,args):
+    # Unpack inputs
     n_e=x[0]*1E21
     T_e=x[1]
     N_n=x[2]*1E22
+    
+    Id, mdot, Te_ins = args
+    
+    # Compute goal vector
     goal=np.zeros(3)
-    goal[0]=power_balance(n_e,N_n,T_e,T_e_ins,I_d)/1E6
-    goal[1]=ion_balance(n_e,T_e,N_n)*100
-    goal[2]=flow_balance(n_e,N_n,T_e,F)*100
+
+    goal[0] = power_balance(n_e,N_n,T_e,T_e_ins,I_d)
+    goal[1]=ion_balance(n_e,T_e,N_n)   
+    goal[2]=flow_balance(n_e,N_n,T_e,F)
+    
+    # Rescale goal vector
+    goal[0] /= 1e6
+    goal[1] *= 100
+    goal[2] *= 100
     
     return goal
 
@@ -322,64 +134,94 @@ def solve(do, Lo,
         the factor of 5/2 ('corrected')
     """   
     
+    if(len(Id) == len(mdot) == len(TeV_ins)):
+        cases = zip(Id,mdot,TeV_ins)
+    else:
+        cases = product(Id,mdot,TeV_ins)
 
+    solvec = []
+    for lId,lmdot,lTeV_ins in cases:
+        # Initial guess: densities are scaled
+        ne0 = 1e21 # 1/m3
+        Te0 = 2 # eV
+        ng0 = 1e22 # 1/m3
+        
+        x0 = np.array([ne0*1e-21,Te0,ng0*1e-22])
+    
 
-### Operating condition
-Idvec = np.arange(1.,10.,0.1) # A
-mdotvec = np.array([1,6,10]) # sccm
+        root_options = {'maxiter':int(1e6),
+                        'xtol':solver_tol,'ftol':solver_tol}
 
-### Storage for all solutions
-# 3 mass flow rates
-# size(Idvec) currents
-# store ne,ng,Te,convergence
-solvec = np.zeros([3,np.size(Idvec),4])
+        # Arguments
+        args = [lId,lmdot,lTeV_ins]
+        
+        # Solve!
+        optimize_results = root(goal_function,x0,args=args,
+                                method='lm',options = root_options)
+        
+        # Extract and rescale results
+        ne,TeV,ng = optimize_results.x
+        
+        rescaled_results = [ne*1e21,Te0,ng*1e22]
+        
+        solvec.append(rescaled_results)        
 
-
-#x0 = np.array([1,2.5,1])
-x0vec = np.array( [ [1,2.5,1],[2,1.8,2],[0.5,1.7,1]])
-TeInsvec = np.array([0.848866477452,0.779318325967,0.791571666652])
-
-#mdot_idx = 0
-#Id_idx = 0
-#filename = 'mandell_solvec_mdot-'
-#for mdot in np.nditer(mdotvec):
-#		print mdot
-#		x0 = x0vec[mdot_idx]
-#		filename += str(mdot)
-#		Id_idx = 0
+#### Operating condition
+#Idvec = np.arange(1.,10.,0.1) # A
+#mdotvec = np.array([1,6,10]) # sccm
 #
-#		#T_e_ins = TeInsvec[mdot_idx]
-#		T_e_ins = 0.8
+#### Storage for all solutions
+## 3 mass flow rates
+## size(Idvec) currents
+## store ne,ng,Te,convergence
+#solvec = np.zeros([3,np.size(Idvec),4])
 #
-#		for Id in np.nditer(Idvec):
-#				data = (T_e_ins,Id,mdot) 
-#				optimize_results = root(zerofun,x0,data,method='lm',options={'maxiter':1000000,'xtol':1e-8,'ftol':1e-8})
-#				n_e,T_e,N_n = optimize_results.x
-#				n_e *= 1E21
-#				N_n *= 1E22
-#				solvec[mdot_idx,Id_idx,:] = [n_e,N_n,T_e,optimize_results.success]
-#				Id_idx += 1
 #
-#		np.save(filename,solvec[mdot_idx,:,:])
-#		mdot_idx += 1
-#		filename = 'mandell_solvec_mdot-'
+##x0 = np.array([1,2.5,1])
+#x0vec = np.array( [ [1,2.5,1],[2,1.8,2],[0.5,1.7,1]])
+#TeInsvec = np.array([0.848866477452,0.779318325967,0.791571666652])
 #
-#np.save('mandell_solvec-all',solvec)
-x0 = np.array([1.0,1.6,1])
-data = (T_e_ins,3.26,6)
-optimize_results = root(zerofun,x0,data,method='lm',options={'maxiter':1000000,'xtol':1e-8,'ftol':1e-8})
-n_e,T_e,N_n = optimize_results.x
-n_e *= 1e21
-N_n *= 1e22
-print n_e,T_e,N_n
-
-#ion_output = pi*r**2*J_i(n_e,T_e)
-#ion_eff = ion_output/(resistance(n_e,T_e,N_n)*I_d**2)
-#utilization = ion_output/(0.0718*F)
-#voltage_drop = resistance(n_e,T_e,N_n)*I_d
-
-#print('Electron Density\tElectron Temperature\tNeutral Density')
-#print(str(n_e)+'\t'+str(T_e)+'\t\t'+str(N_n))
-#print('Orifice Vd\t\tIon Output\t\tIon Output/Power\tUtilitzation')
-#print(str(voltage_drop)+'\t\t'+str(ion_output)+'\t\t'+str(ion_eff)+'\t'+str(utilization))
+##mdot_idx = 0
+##Id_idx = 0
+##filename = 'mandell_solvec_mdot-'
+##for mdot in np.nditer(mdotvec):
+##		print mdot
+##		x0 = x0vec[mdot_idx]
+##		filename += str(mdot)
+##		Id_idx = 0
+##
+##		#T_e_ins = TeInsvec[mdot_idx]
+##		T_e_ins = 0.8
+##
+##		for Id in np.nditer(Idvec):
+##				data = (T_e_ins,Id,mdot) 
+##				optimize_results = root(zerofun,x0,data,method='lm',options={'maxiter':1000000,'xtol':1e-8,'ftol':1e-8})
+##				n_e,T_e,N_n = optimize_results.x
+##				n_e *= 1E21
+##				N_n *= 1E22
+##				solvec[mdot_idx,Id_idx,:] = [n_e,N_n,T_e,optimize_results.success]
+##				Id_idx += 1
+##
+##		np.save(filename,solvec[mdot_idx,:,:])
+##		mdot_idx += 1
+##		filename = 'mandell_solvec_mdot-'
+##
+##np.save('mandell_solvec-all',solvec)
+#x0 = np.array([1.0,1.6,1])
+#data = (T_e_ins,3.26,6)
+#optimize_results = root(zerofun,x0,data,method='lm',options={'maxiter':1000000,'xtol':1e-8,'ftol':1e-8})
+#n_e,T_e,N_n = optimize_results.x
+#n_e *= 1e21
+#N_n *= 1e22
+#print n_e,T_e,N_n
+#
+##ion_output = pi*r**2*J_i(n_e,T_e)
+##ion_eff = ion_output/(resistance(n_e,T_e,N_n)*I_d**2)
+##utilization = ion_output/(0.0718*F)
+##voltage_drop = resistance(n_e,T_e,N_n)*I_d
+#
+##print('Electron Density\tElectron Temperature\tNeutral Density')
+##print(str(n_e)+'\t'+str(T_e)+'\t\t'+str(N_n))
+##print('Orifice Vd\t\tIon Output\t\tIon Output/Power\tUtilitzation')
+##print(str(voltage_drop)+'\t\t'+str(ion_output)+'\t\t'+str(ion_eff)+'\t'+str(utilization))
 
