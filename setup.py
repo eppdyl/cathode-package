4bd12ab93d4ebb9c3b0437a1f862433301db397d
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2019-03-07 14:12:48 -0500

    Update to v1.0

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
-setup(name='cathode', 
-      version = '0.1',
-      description = 'Hollow cathode modeling package',
-      author = 'EPPDyL',
-      license = 'None',
+setup(name='cathode',
+      version='1.0',
+      description='Hollow cathode modeling package',
+      author='EPPDyL',
+      license='None',
       packages=find_packages(),
-      install_requires=['numpy','scipy'],
-      zip_safe = False)
+      install_requires=['numpy', 'scipy'],
+      zip_safe=False)
