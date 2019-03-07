85e293c3586d85decda4ea34fd3e4f9ef74fc598
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2019-03-07 13:47:25 -0500

    Clean up w/ PEP257

--- cathode/collisions/reaction_rate.py
+++ cathode/collisions/reaction_rate.py
@@ -30,63 +30,68 @@ This submodule contains functions related to the computation of reaction rates.
 
 import numpy as np
 
+import cathode.constants as cc
+from scipy.integrate import quad
+
 ###############################################################################
 #                     Reaction Rate Integrals
 ###############################################################################
-
 @np.vectorize
-def reaction_rate(xsec_spline,TeV,Emin=None,Emax=None,output_xsec=False):
+def reaction_rate(xsec_spline, TeV, Emin=None, Emax=None, output_xsec=False):
     """
     Returns the reaction rate for the process with cross section xsec_spline
-    for Maxwellian electrons with temperature TeV.  Emin and Emax specify the 
+    for Maxwellian electrons with temperature TeV.  Emin and Emax specify the
     minimum and maximum energy for integration (Emin should be the threshold
     energy for the process, Emax should be the useful limit of the spline)
     Inputs:
-        Cross section spline, created by create_cross_section_spline() (CrossSection object)
-        Electron temperature, eV
-        Minimum energy, eV (threshold for process of interest)
-        Maximum energy, eV (limit of data or maximum extrapolation for spline)
+        - xsec_spline: cross section spline, created by
+        create_cross_section_spline() (CrossSection object)
+        - TeV: electron temperature (eV)
+        - Emin: minimum energy, i.e. threshold for process of interest (eV)
+        - Emax: maximum energy, i.e. limit of data or maximum extrapolation
+        for spline (eV)
     Optional Input:
-        output_xsec can be set to true to return a tuple of (reaction rate, cross section)
-            Defaults to FALSE, -> outputs only reaction rate unless specified
+        - output_xsec: can be set to true to return a tuple of
+        (reaction rate, cross section). Defaults to FALSE, in which case it
+        outputs only reaction rate.
     Outputs:
-        if output_xsec is true, (reaction rate, cross section) (m^3/s, m^2)
-        otherwise, (reaction rate) (m^3/s)
+        - output_xsec true: (reaction rate, cross section) (m^3/s, m^2)
+        - output_xsec false: (reaction rate) (m^3/s)
     """
-    
-    #check whether integration bounds have been specified, if not, 
-    #use minimum and maximum bounds for spline data (flux integral should always be
-    #evaluated from 0 eV)
+    # Check whether integration bounds have been specified, if not, use
+    # minimum and maximum bounds for spline data (flux integral should always
+    # be evaluated from 0 eV)
     if not Emin:
         Emin = np.min(xsec_spline.Emins)
     if not Emax:
         Emax = np.max(xsec_spline.Emaxs)
-    
-    #normalization factor for reaction rate integral
-    normalization = (8.0*np.pi*cc.e**2.0/np.sqrt(cc.me))/(
-            (2.0*np.pi*cc.e*TeV)**(3.0/2.0))
-    
+
+    # Normalization factor for reaction rate integral
+    normalization = 8*np.pi*cc.e**2/np.sqrt(cc.me)
+    normalization /= (2*np.pi*cc.e*TeV)**(3/2)
+
     scaling = np.max(xsec_spline.scalings)
-    
-    #define integrand lambda functions
+
+    # Define integrand lambda functions
     energy_integrand = lambda E: E*xsec_spline(E)*np.exp(-E/TeV)*scaling
     flux_integrand = lambda E: E*np.exp(-E/TeV)
-    
-    #integrate (note that these return the error estimate as the second output)
-    energy_integral = quad(energy_integrand,Emin,Emax,epsabs=1.0E-30,limit=5000)
-    flux_integral = quad(flux_integrand,0.0,Emax,epsabs=1.0E-30,limit=5000)
-    
-    #reaction rate
+
+    # Integrate (note that these return the error estimate as the second output)
+    energy_integral = quad(energy_integrand, Emin, Emax, epsabs=1.0E-30,
+                           limit=5000)
+    flux_integral = quad(flux_integrand, 0.0, Emax, epsabs=1.0E-30, limit=5000)
+
+    # Reaction rate
     K = normalization*energy_integral[0]/scaling
-    
-    #Maxwellian-averaged cross section
+
+    # Maxwellian-averaged cross section
     xsec_avg = energy_integral[0]/flux_integral[0]/scaling
-    
+
     #if specified, output both the cross section and reaction rate
     if output_xsec:
-        return K,xsec_avg
+        return K, xsec_avg
     else:
-        return K    
+        return K
 
 
 @np.vectorize
