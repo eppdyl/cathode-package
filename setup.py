c708a1bbbc769a50d190dcfab8f94b6150afd788
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2020-07-15 12:52:25 -0400

    Update license

--- setup.py
+++ setup.py
@@ -1,3 +1,35 @@
+###############################################################################
+#
+# "cathode" Python package
+# Version: 1.0
+# A package of various cathode models that have been published throughout the
+# years. Associated publications:
+# - Taunay, P.-Y. C. R., Wordingham, C. J., and Choueiri, E. Y., "A 0-D model 
+# for orificed hollow cathodes with application to the scaling of total 
+# pressure," AIAA Propulsion and Energy Forum, 2019, AIAA-2019-4246
+# - Wordingham, C. J., Taunay, P.-Y. C. R., and Choueiri, E. Y., "A critical
+# review of hollow cathode modeling: 0-D models," 53rd AIAA/ASME/SAE/ASEE Joint 
+# Propulsion Conference, 2017, AIAA-2017-4888 
+# 
+###############################################################################
+# Copyright (C) 2017-2020 Pierre-Yves Taunay, Chris Wordingham
+#  
+# This file is part of the Python "cathode" package.
+#
+# The Python "cathode" package is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License (LGPL) as 
+# published by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# The Python "cathode" package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License (LGPL) for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License 
+# (LGPL) along with the Python "cathode" package.  
+# If not, see <https://www.gnu.org/licenses/>.
+###############################################################################
 import os
 import fnmatch
 from setuptools import setup, find_packages
@@ -17,7 +49,7 @@ setup(name='cathode',
       version='1.0',
       description='Hollow cathode modeling package',
       author='EPPDyL',
-      license='AGPLv3',
+      license='LGPLv3',
       package_data={'': extra_files},
       packages=find_packages(),
       install_requires=['numpy', 'scipy','h5py'],
